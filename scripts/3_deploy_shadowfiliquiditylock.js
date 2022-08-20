e5589726439c8c05d2633e02fe37457283494aa4
spreadmycode <spreadmycode@outlook.com>
2022-08-19 21:08:31 -0700

    token address changed

--- scripts/3_deploy_shadowfiliquiditylock.js
+++ scripts/3_deploy_shadowfiliquiditylock.js
@@ -8,7 +8,7 @@ async function main() {
   );
   const contract = await ShadowFiLiquidityLock.deploy(
     "0x9ac64cc6e4415144c455bd8e4837fea55603e5c3",
-    "0x6ac5BE595D791e6a547Ae8753000bC3b2731E5cA",
+    "0xa31111C45976d9D3dF22483af98bab8226e37c8C",
     1661147602
   );
 
