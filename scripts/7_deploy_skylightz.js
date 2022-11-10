211081d6b4669481d23fc13a2ed4c6918ab9d376
spreadmycode <spreadmycode@outlook.com>
2022-11-09 17:59:38 -0800

    skylightz nft metadata updated

--- scripts/7_deploy_skylightz.js
+++ scripts/7_deploy_skylightz.js
@@ -5,8 +5,8 @@ const hre = require("hardhat");
 async function main() {
   const Skylightz = await hre.ethers.getContractFactory("Skylightz");
   const contract = await Skylightz.deploy(
-    "https://skylightz.mypinata.cloud/ipfs/QmRcVVxcXtkJzUcoGC88jLCHDYbz8i9KPRWdP7c8zSiH2j/",
-    "https://skylightz.mypinata.cloud/ipfs/QmVFyq23yDDWwjpBxQuxGy3N5iCAM6GNnCSQSjEVAYze2x/"
+    "https://skylightz.mypinata.cloud/ipfs/QmPmjMrpHEug7LP5GYXAyAWapQMVoWQeH1U1xAh4dLa6tK/",
+    "https://skylightz.mypinata.cloud/ipfs/Qmd87igWj7bJhFExEzKPJAcmpvrUnGkTNuCwX8AeCGqQsA/"
   );
 
   await contract.deployed();
